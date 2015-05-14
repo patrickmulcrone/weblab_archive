7ad2e7635f7b058e338ecf4b8367459af354c119
Patrick Mulcrone <patrickmulcrone@gmail.com>
2015-05-13 21:26:09 -0600

    test 1

---
+++ php/searchResults.php
@@ -0,0 +1,18 @@
+<?php
+
+
+<!-- search results -->
+  <div class="row">
+  		<div class="col-xs-12 col-sm-6 col-md-3"></div>
+      <div class="col-xs-12 col-sm-6 col-md-3"></div>
+      <div class="col-xs-12 col-sm-6 col-md-3"></div>
+      <div class="col-xs-12 col-sm-6 col-md-3"></div>
+      <div class="col-xs-12 col-sm-6 col-md-3"></div>
+      <div class="col-xs-12 col-sm-6 col-md-3"></div>
+      <div class="col-xs-12 col-sm-6 col-md-3"></div>
+      <div class="col-xs-12 col-sm-6 col-md-3"></div>
+      <div class="col-xs-12 col-sm-6 col-md-3"></div>
+      <div class="col-xs-12 col-sm-6 col-md-3"></div>
+  	</div>
+
+    ?>
